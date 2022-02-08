ebead943feec914f1f9d0610026738c502e5bbd9
James Cavanagh <james.cavanagh@oneadvanced.com>
2022-02-08 12:06:24 +0000

    feat: update layout

--- src/_includes/js/navigation.js
+++ src/_includes/js/navigation.js
@@ -1,27 +1,27 @@
 const storeScrollPosition = () => {
   // save current menu scroll position in session storage
-  const sidebar = document.querySelector('mcc-sheet aside > .mcc-menu')
+  const sidebar = document.querySelector('aside > .mcc-menu')
   sessionStorage.setItem('MDS_ScrollTop', sidebar.scrollTop)
 }
 
 const setScrollPosition = () => {
   // read menu scroll position from storage and apply
   const scrollTop = sessionStorage.getItem('MDS_ScrollTop')
-  const sidebar = document.querySelector('mcc-sheet aside > .mcc-menu')
+  const sidebar = document.querySelector('aside > .mcc-menu')
   sidebar.scrollTop = scrollTop
 }
 
 const highlightCurrentPage = () => {
   // highlight current page in nav sidebar
   const currentPath = window.location.pathname
-  const navButton = document.querySelector(`mcc-sheet aside > .mcc-menu a[href="${currentPath}"]`)
+  const navButton = document.querySelector(`aside > .mcc-menu a[href="${currentPath}"]`)
   navButton.classList.add('mu-is-active')
   navButton.closest('details').open = true
   navButton.scrollIntoView({ behavior: 'smooth', block: 'nearest' })
 }
 
 document.addEventListener('DOMContentLoaded', _ => {
-  document.querySelectorAll('mcc-sheet aside > .mcc-menu a')
+  document.querySelectorAll('aside > .mcc-menu a')
     .forEach(a => {
       a.addEventListener('click', _ => {
         storeScrollPosition()
